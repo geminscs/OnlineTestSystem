014cc40e106538d204f1575a385c1dfb1b728583
min_g <675980887@qq.com>
2015-04-09 16:58:30 +0800

    增加spring 框架

--- src/main/java/com/train/controllers/LoginController.java
+++ src/main/java/com/train/controllers/LoginController.java
@@ -1,5 +1,23 @@
 package com.train.controllers;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
 public class LoginController {
 
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public void env(HttpServletResponse response) throws IOException {
+	    response.setContentType("text/plain");
+	    PrintWriter out = response.getWriter();
+	    out.println("System Environment:\n");
+	}
 }
