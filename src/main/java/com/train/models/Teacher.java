43598410da2c3226ecf871df7a4bf8c7b7d888a0
min_g <675980887@qq.com>
2015-04-10 10:02:19 +0800

    12

---
+++ src/main/java/com/train/models/Teacher.java
@@ -0,0 +1,58 @@
+package com.train.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="teacher")
+public class Teacher {
+	@Id
+	@Column(name="account")
+	private String account;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="password")
+	private String password;
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Teacher(String account, String name, String password) {
+		super();
+		this.account = account;
+		this.name = name;
+		this.password = password;
+	}
+
+	public Teacher() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
